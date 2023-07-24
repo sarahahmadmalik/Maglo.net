e4bfdf222840818cc842aa56eab13b7b1c97a254
sarahahmadmalik <sarahahmadmalik55@gmail.com>
2023-07-25 00:02:57 +0500

    post-a-comment-modified

--- components/ReplyForm.js
+++ components/ReplyForm.js
@@ -12,13 +12,16 @@ const ReplyForm = () => {
     email: "",
     website: "",
     comment: "",
+    rememberMe: false,
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, type, checked } = e.target;
+    const fieldValue = type === "checkbox" ? checked : value;
+
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value,
+      [name]: fieldValue,
     }));
   };
 
@@ -30,12 +33,13 @@ const ReplyForm = () => {
       email: "",
       website: "",
       comment: "",
+      rememberMe: false, 
     });
   };
 
   return (
     <div className="w-full px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16">
-      <h2 className={` ${font2.className} text-[1.5rem] md:text-[1.6rem] xl:text-[1.9rem] font-bold mb-2`}>Leave a Reply</h2>
+      <h2 className={`${font2.className} text-[1.5rem] md:text-[1.6rem] xl:text-[1.9rem] font-bold mb-2`}>Leave a Reply</h2>
       <p className="mb-4 text-sm">
         Your email address will not be published. Required fields are marked *
       </p>
@@ -88,6 +92,19 @@ const ReplyForm = () => {
             rows={6}
             className="w-full p-2 bg-[#E2EEF8] rounded text-sm resize-none"
           />
+        </div>
+        <div className="mt-4 flex items-center">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            name="rememberMe"
+            checked={formData.rememberMe}
+            onChange={handleChange}
+            className="mr-2"
+          />
+          <label htmlFor="rememberMe" className="text-sm">
+            Save my name, email, and website in this browser for the next time I comment.
+          </label>
         </div>
         <div className="mt-6">
           <button
